35183c1081f867c80b1c46542c79afba677d0f50
hanjhou2000716 <hanjhou2000716@gmail.com>
2026-07-20 15:21:36 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // 如果之後要加 Tailwind 設定
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
