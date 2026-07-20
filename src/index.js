8b937a540ccff12d9d8f668907e0bfdeedb2cb31
hanjhou2000716 <hanjhou2000716@gmail.com>
2026-07-20 15:38:02 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // 如果之後要加 Tailwind 設定
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
